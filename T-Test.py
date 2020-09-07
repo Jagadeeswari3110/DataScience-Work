4ee7d58af48998eab63372f4fbeb6299f2c6d1dd
Jagadeeswari <Jagadeeswari3110@users.noreply.github.com>
2020-09-07 22:38:17 +0530

    Add files via upload

---
+++ T-Test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from scipy import stats
+
+
+# In[ ]:
+
+
+#stats.norm.cdf()
+
+
+# In[2]:
+
+
+stats.t.cdf(2.23,df=79)
+
+
+# In[ ]:
+
+
+
+
